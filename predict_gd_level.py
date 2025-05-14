a6075399968a5e14c757094fdcb654b9b531be94
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-05-14 18:05:28 -0500

    Update predict_gd_level.py

--- predict_gd_level.py
+++ predict_gd_level.py
@@ -1,41 +1,40 @@
 import numpy as np
+import json
 import tensorflow as tf
 from keras.src.legacy.preprocessing.image import image_utils
-import sys
-
-# Cargar el modelo entrenado
-model = tf.keras.models.load_model('gd_level_classifier.h5')
+from keras.src.applications.mobilenet_v2 import preprocess_input
 
 # Diccionario de clases (puedes ajustar según los nombres reales de tus carpetas)
-class_indices = {
-    'Back On Track': 0,
-    'Polargeist': 1,
-    'Stereo Madness': 2
-}
+with open("class_indices.json", "r") as f:
+    class_indices = json.load(f)
 
 # Invertir el diccionario para obtener el nombre desde el índice
 index_to_class = {v: k for k, v in class_indices.items()}
 
-img_path = "testing/2872315073_preview_2.jpg"
+img_path = "testing/dash.jpg"
 
 # Cargar y preparar la imagen
-img = image_utils.load_img(img_path, target_size=(128, 128))
-img_array = image_utils.img_to_array(img) / 255.0
+img = image_utils.load_img(img_path, target_size=(160, 160))
+img_array = image_utils.img_to_array(img)
+img_array = preprocess_input(img_array)
 img_array = np.expand_dims(img_array, axis=0)
 
+def reload_model(model_path='gd_level_classifier.keras'):
+    return tf.keras.models.load_model(model_path)
+model = reload_model()
+print(model.summary())
+
 # Predecir
-prediction = model.predict(img_array)[0]  # Primera (y única) predicción
-predicted_index = np.argmax(prediction)
-confidence = prediction[predicted_index] * 100
+prediction = model.predict(img_array)[0]
 
 print(f"\n📷 Imagen: {img_path}")
+print("🎯 Probabilidades por clase:\n")
 
-confidence_threshold = 50.0
+# Ordenar índices por probabilidad descendente
+sorted_indices = np.argsort(prediction)[::-1]
 
-if confidence < confidence_threshold:
-    print("No se encontró ningún nivel que coincida con la imagen dada")
-else:
-    # Mostrar resultado
-    predicted_class = index_to_class[predicted_index]
-    print(f"✅ El nivel detectado es: **{predicted_class}**")
-    print(f"🔢 Confianza: {confidence:.2f}%")
+# Mostrar todas las clases con su porcentaje
+for i in sorted_indices:
+    class_name = index_to_class[i]
+    prob = prediction[i] * 100
+    print(f"{class_name:30} → {prob:.2f}%")
